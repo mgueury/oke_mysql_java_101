99e5bae3718a97354c75c9c6632360815a2ee747
Oracle Public Cloud User <opc@dev2.sub08301607040.mgsharedvcn.oraclevcn.com>
2022-02-19 21:31:14 +0000

    0.57

--- bin/docker_login.sh
+++ bin/docker_login.sh
@@ -1,5 +1,6 @@
-DIR=$(dirname "$0")
-. $DIR/env.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. $SCRIPT_DIR/env.sh
+
 # Login to docker
 docker login $OCI_REGION -u $OCI_NAMESPACE/$OCI_USERNAME -p "$OCI_TOKEN"
 # OCI Repository prefix
