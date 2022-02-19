dfbe63c799c3186e83ae0dc6e8baf22efc133955
mgueury <mgueury@skynet.be>
2022-02-19 21:34:51 +0100

    Delete env.sh

--- bin/env.sh
+++
@@ -1,8 +0,0 @@
-OCI_REGION=fra.ocir.io
-OCI_NAMESPACE=fr03kzmuvhtf
-OCI_EMAIL=marc.gueury@oracle.com
-OCI_USERNAME=oracleidentitycloudservice/marc.gueury@oracle.com
-OCI_TOKEN="7(+kqj46qEK682_yCskI"
-MYSQL_IP=10.1.1.237
-MYSQL_USER=root
-MYSQL_PASSWORD=Welcome1!
