6e7dc796a6f3c47643dc1cd5c9bceb1d27ee70fd
Oracle Public Cloud User <opc@dev2.sub08301607040.mgsharedvcn.oraclevcn.com>
2022-02-22 15:01:34 +0000

    0.511

--- demo2/v1/bin/build.sh
+++ demo2/v1/bin/build.sh
@@ -1,3 +1,6 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. $SCRIPT_DIR/../../../bin/env.sh
+
 ./mvnw clean package
 docker build -t webquerydb:v1 .
 docker images | grep webquerydb
--- demo2/v1/bin/push.sh
+++ demo2/v1/bin/push.sh
@@ -1,3 +1,5 @@
-. ../../bin/docker_login.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. $SCRIPT_DIR/../../../bin/docker_login.sh
+
 docker tag webquerydb:v1 $DOCKER_PREFIX/webquerydb:v1
 docker push $DOCKER_PREFIX/webquerydb:v1
