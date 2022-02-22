d1823a4bf018025ec74d7827baefb00dcc160634
Oracle Public Cloud User <opc@dev2.sub08301607040.mgsharedvcn.oraclevcn.com>
2022-02-22 14:18:35 +0000

    0.592

--- bin/create_registy_secret.sh
+++ bin/create_registy_secret.sh
@@ -1,2 +1,4 @@
-. ./env.sh
-kubectl create secret docker-registry ocirsecret --docker-server=$OCI_REGION --docker-username="$OCI_NAMESPACE/$OCI_USERNAME' --docker-password="$OCI_TOKEN" --docker-email="$OCI_EMAIL"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. $SCRIPT_DIR/env.sh
+
+kubectl create secret docker-registry ocirsecret --docker-server=$OCI_REGION --docker-username="$OCI_NAMESPACE/$OCI_USERNAME" --docker-password="$OCI_TOKEN" --docker-email="$OCI_EMAIL"
