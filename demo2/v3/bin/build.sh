beff0eb98e26d99e58e0e603f0692e3b89895f58
Oracle Public Cloud User <opc@dev2.sub08301607040.mgsharedvcn.oraclevcn.com>
2022-06-14 18:55:57 +0000

    native

--- demo2/v3/bin/build.sh
+++ demo2/v3/bin/build.sh
@@ -1,6 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . $SCRIPT_DIR/../../../bin/env.sh
 
+export PATH=$GRAALVM_HOME/bin:$PATH
 ./mvnw clean package -Pdistroless
 docker build -t webquerydb-native:v3 .
 docker images | grep webquerydb-native
